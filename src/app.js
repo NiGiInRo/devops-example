1b25a84ff20d9d64e757b9acbe0857fb8434b132
unknown <n.infanter@uniandes.edu.co>
2026-01-14 23:06:18 -0500

    devops: implementacion de docker, metricas con prometeus

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ const express = require('express');
 
 const healthRoutes = require('./routes/health');
 const taskRoutes = require('./routes/tasks');
+const { router: metricsRoutes, metricsMiddleware } = require('./routes/metrics');
 
 const app = express();
 
 app.use(express.json());
+app.use(metricsMiddleware);
 app.use(healthRoutes);
 app.use(taskRoutes);
+app.use(metricsRoutes);
 
 module.exports = app;
